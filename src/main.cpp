3184a87212a25898c7231811f7c8884131ebdb98
Faruk Ertürk <ertuerk.faruk.business@gmail.com>
2025-06-07 19:51:09 +0300

    improvements

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,16 @@ const char *targetUrl = "http://www.example.com/";
 
 void testWebsite()
 {
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    return;
+  }
   LOGService::divider("Website Test");
-
   LOGService::info("URL: %s\n", targetUrl);
-
   HTTPClient http;
   WiFiClient client;
-
   http.begin(client, targetUrl);
-
   int httpCode = http.GET();
-
   if (httpCode > 0)
   {
     if (httpCode == HTTP_CODE_OK)
@@ -43,17 +42,14 @@ void testWebsite()
   {
     LOGService::error("HTTP GET request failed! Error: %s\n", http.errorToString(httpCode).c_str());
   }
-
   http.end();
+  WiFi.disconnect();
   LOGService::divider("Website Test Completed");
 }
 
-void test()
+void testLED()
 {
-  LOGService::divider("Testing");
-  delay(1000);
   LOGService::divider("LED Test");
-  delay(1000);
   LOGService::info("LED ON\n");
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -72,18 +68,16 @@ void test()
   delay(1000);
   LOGService::info("LED OFF\n");
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-
   LOGService::divider("LED Test Completed");
-  LOGService::divider("WiFi Test");
+}
 
+void testWiFi()
+{
+  LOGService::divider("WiFi Test");
   String ssid = ROMService::readString(SSID_START_ADDR, SSID_MAX_LEN);
   String password = ROMService::readString(PASS_START_ADDR, PASS_MAX_LEN);
-
   bool connected = false;
-
   WiFi.mode(WIFI_STA);
-
   if (ssid.length() > 0 && password.length() > 0)
   {
     LOGService::info(("Attempting to connect with saved credentials: SSID=" + ssid + "\n").c_str());
@@ -96,9 +90,7 @@ void test()
       LOGService::drawProgressBar(difference, WIFI_TIMEOUT_MS, 50);
       delay(1000);
     }
-
     LOGService::drawProgressBar(WIFI_TIMEOUT_MS, WIFI_TIMEOUT_MS, 50);
-
     if (WiFi.status() == WL_CONNECTED)
     {
       connected = true;
@@ -106,25 +98,18 @@ void test()
     else
     {
       LOGService::info("Failed to connect with saved credentials.\n");
-      WiFi.disconnect();
     }
   }
-
   if (!connected)
   {
     LOGService::info("Enter SSID:\n");
-
     ssid = CommandService::waitForInput();
-
     LOGService::info("Enter Password:\n");
-
     password = CommandService::waitForInput();
-
     ROMService::writeString(SSID_START_ADDR, ssid, SSID_MAX_LEN);
     ROMService::writeString(PASS_START_ADDR, password, PASS_MAX_LEN);
     LOGService::info("Credentials saved to ROM.\n");
     WiFi.begin(ssid.c_str(), password.c_str());
-
     unsigned long startTime = millis();
     unsigned long difference = millis() - startTime;
     while (WiFi.status() != WL_CONNECTED && ((difference = millis() - startTime) < WIFI_TIMEOUT_MS))
@@ -132,10 +117,8 @@ void test()
       LOGService::drawProgressBar(difference, WIFI_TIMEOUT_MS, 50);
       delay(1000);
     }
-
     LOGService::drawProgressBar(WIFI_TIMEOUT_MS, WIFI_TIMEOUT_MS, 50);
   }
-
   if (WiFi.status() == WL_CONNECTED)
   {
     LOGService::success("WiFi Connected!\n");
@@ -143,28 +126,27 @@ void test()
     LOGService::info(("MAC Address: " + WiFi.macAddress() + "\n").c_str());
     LOGService::info("RSSI: %d dBm\n", WiFi.RSSI());
     LOGService::divider("WiFi Test Completed");
-
-    testWebsite();
-
-    WiFi.disconnect();
   }
   else
   {
     LOGService::error("WiFi not Connected!\n");
-    WiFi.disconnect();
     LOGService::divider("WiFi Test Completed");
   }
+}
 
-  delay(1000);
+void test()
+{
+  LOGService::divider("Testing");
+  testLED();
+  testWiFi();
+  testWebsite();
   LOGService::divider("Test Completed");
 }
 
 void setup()
 {
   Serial.begin(115200);
-
   delay(1000);
-
   Serial.println();
   LOGService::divider("Booting");
   ROMService::initialize(EEPROM_SIZE);
@@ -176,7 +158,6 @@ void setup()
 void loop()
 {
   String command = CommandService::waitForInput();
-
   if (command == "test")
   {
     test();
@@ -191,6 +172,5 @@ void loop()
   {
     LOGService::info("Unknown command: %s", command);
   }
-
   LOGService::info("Enter command:\n");
 }
